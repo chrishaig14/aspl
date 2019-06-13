372f2e6fe05e755dd39f1edf9a11bd40493ecef5
chrishaig14 <chrishaig14@gmail.com>
2019-06-12 21:50:43 -0300

    Refactor & modularise scanner
    Add line and column number to tokens

--- scanner.py
+++ scanner.py
@@ -1,61 +1,94 @@
-def scan(str, cur):
-    if str[cur].isalpha():
-        # print("starts with a letter")
-        start = cur
-        while cur < len(str) and str[cur].isalnum():
-            cur += 1
-        end = cur
-        if str[start:end] == "var":
-            return ("var"), start, cur
-        return ("id", str[start:end]), start, cur
-    elif str[cur].isdigit():
-        # print("starts with a digit")
-        start = cur
-        while cur < len(str) and str[cur].isdigit():
-            cur += 1
-        end = cur
-        return ("integer", int(str[start:end])), start, cur
-    elif str[cur].isspace():
-        start = cur
-        while cur < len(str) and str[cur].isspace():
-            cur += 1
-        return "", start, cur
-    elif str[cur] == ";":
+keywords = ["var", "fun", "return", "if", "else"]
+ops = ["++", "--", "+", "-", "*", "/", "=", "=="]
+special = [";"]
+
+
+def scan_alnum(str, cur, line, col):
+    start = cur
+    while cur < len(str) and str[cur].isalnum():
+        cur += 1
+        col += 1
+    end = cur
+    token = str[start:end]
+    if token in keywords:
+        return (token), start, cur, line, col
+    return ("id", str[start:end]), start, cur, line, col
+
+
+def scan_num(str, cur, line, col):
+    start = cur
+    while cur < len(str) and str[cur].isdigit():
+        cur += 1
+        col += 1
+
+    end = cur
+    return ("integer", int(str[start:end])), start, cur, line, col
+
+
+def scan_other(str, cur, line, col):
+    if str[cur] == ";":
         start = cur
         cur += 1
-        return "semic", start, cur
+        col += 1
+        return "semic", start, cur, line, col
     elif str[cur] == "=":
         start = cur
         if str[cur + 1] != "=":
             cur += 1
-            return "assign", start, cur
+            col += 1
+            return "assign", start, cur, line, col
         else:
             cur += 2
-            return "eqop", start, cur
+            col += 2
+            return "eqop", start, cur, line, col
     elif str[cur] == "+":
         start = cur
         if str[cur + 1] != "+":
             cur += 1
-            return "plus", start, cur
+            col += 1
+            return "plus", start, cur, line, col
         else:
             cur += 2
-            return "incrop", start, cur
+            col += 2
+            return "incrop", start, cur, line, col
     else:
-        return "invalid", cur, cur
+        return "invalid", cur, cur, line, col
+
+
+def scan(str, cur, line, col):
+    if str[cur].isalpha():
+        return scan_alnum(str, cur, line, col)
+    elif str[cur].isdigit():
+        return scan_num(str, cur, line, col)
+    elif str[cur].isspace():
+        start = cur
+        while cur < len(str) and str[cur].isspace():
+            if str[cur] == "\n":
+                line += 1
+                col = 1
+            else:
+                col += 1
+            cur += 1
+        return "", start, cur, line, col
+    else:
+        return scan_other(str, cur, line, col)
 
 
 f = open("sample1.aspl")
 text = f.read()
 cur = 0
+line = 1
+col = 1
 while cur < len(text):
-    x = scan(text, cur)
-    # print("scan returned ",x)
-    token, start, cur = x
+    x = scan(text, cur, line, col)
+    token, start, cur, line, new_col = x
     if token == "invalid":
         print("ERROR: invalid token at", cur, ":" + text[cur])
         break
     else:
-        if token == "":
-            continue
-        print(str(token) + " at " + str(start))
-# print(result)
+        # if token == "":
+        #     continue
+        if token != "":
+            print(str(token) + " at " + str(start), " line: ", line, " col: ", col)
+    col = new_col
+    # print("col now is: ",)
